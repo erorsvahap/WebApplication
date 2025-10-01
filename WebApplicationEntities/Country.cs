aafa0dad6e322a124dd6ab83e18f2c082087936b
test <test@test.com.tr>
2025-10-01 14:56:42 +0300

    dfsdf

---
+++ WebApplicationEntities/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplicationEntities
+{
+    public class Country : BaseEntity
+    {
+        public Country() { }
+
+        [DbIdentity]
+        [DbColumnName("UlkeId")]
+        public int Id { get; set; }  // Identity kolon ve kolon imi sql deki gerçek ismi tut
+        public override string TableName => "ULKE";
+
+        public string UlkeAd { get; set; }
+
+
+    }
+}
---
+++ WebApplicationEntities/District.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplicationEntities
+{
+    public class District : BaseEntity
+    {
+        public override string TableName => "Ilce";
+        public int IlId { get; set; }
+        public string IlceAd { get; set; }
+
+        [DbIdentity]
+        [DbColumnName("IlceId")]
+        public int Id { get; set; }
+    }
+}
